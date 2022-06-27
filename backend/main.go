1116edbc8144ee8b34db98130f73f343292ea455
rktmeister <dylan.raharja@gmail.com>
2022-06-28 00:34:23 +0800

    Playing around with main.go

--- backend/main.go
+++ backend/main.go
@@ -1,53 +1,60 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"io/ioutil"
 )
 
-type hotel struct {
+type User struct {
+	Username string
+	Password string
+}
+
+var Users []*User
+
+type Hotel struct {
 	Name  string `json:"name"`
 	City  string `json:"city"`
 	Price int    `json:"price"`
 }
 
-type destination struct {
-	Name string `json:"name"`
-}
-
-var hotels = []hotel{
-	{"Hotel A", "New York", 100},
-	{"Hotel B", "Los Angeles", 200},
-	{"Hotel C", "Chicago", 300},
-	{"Hotel D", "Las Vegas", 400},
-}
-
-var destinations = []destination{
-	{"New York"},
-	{"Los Angeles"},
-	{"Chicago"},
-	{"Las Vegas"},
-}
-
-func getDestinations(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"destinations": destinations,
-	})
-}
-
-func getHotels(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"hotels": hotels,
-	})
+type Destination struct {
+	dest  string `json:"name"`
+	uid   string `json:"uid"`
+	lat   string `json:"lat"`
+	lng   string `json:"lng"`
+	state string `json:"state"`
 }
 
 func main() {
 	r := gin.Default()
+	r.RedirectTrailingSlash = true
+
+	// Reads destination.json and stores it into array
+	content, err := ioutil.ReadFile("./destination.json")
+	if err != nil {
+		log.Fatal("Error opening file: ", err)
+	}
+
+	var payload Destination
+	err = json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal("Error unmarshalling: ", err)
+	}
 
 	// GET FUNCTIONS
-	r.GET("/hotels", getHotels)
-	r.GET("/destinations", getDestinations)
+	// r.GET("/hotels", getHotels)
+	// r.GET("/destinations", getDestinations)
+
+	// Handles No Route
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{})
+	})
 
 	// POST FUNCTIONS
 
