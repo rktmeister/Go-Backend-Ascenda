d7d8be6d07e7c136fe4b3669e198ec7a98cca234
rktmeister <dylan.raharja@gmail.com>
2022-07-18 22:54:55 +0800

    All redisdb functions working

--- backend/go.mod
+++ backend/go.mod
@@ -4,13 +4,16 @@ go 1.18
 
 require (
 	github.com/RediSearch/redisearch-go v1.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/gomodule/redigo v1.8.8 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -18,6 +21,7 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nitishm/go-rejson/v4 v4.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
 
-	"github.com/RediSearch/redisearch-go/redisearch"
-
-	"github.com/go-redis/redis"
+	"Go-Backend-Ascenda/Backend-API/redisdb"
 
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	ListenAddr = "localhost:3000"
-	RedisAddr  = "localhost:6379"
+	ListenAddr     = "localhost:3000"
+	RedisearchDest = "localhost:6379"
+	RedisearchUser = "localhost:6380"
 )
-
-type User struct {
-	Username string
-	Password string
-}
-
-type Database struct {
-	Client *redis.Client
-}
-
-var Users []*User
 
 type Hotel struct {
 	Name  string `json:"name"`
@@ -38,137 +21,34 @@ type Hotel struct {
 	Price int    `json:"price"`
 }
 
-type Destination struct {
-	Dest string `json:"term"`
-	Uid  string `json:"uid"`
-}
-
-type Uid struct {
-	Uid string `json:"uid"`
-}
-
-// func NewDatabase(address string) (*Database, error) {
-// 	client := redis.NewClient(&redis.Options{
-// 		Addr:     address,
-// 		Password: "",
-// 		DB:       0,
-// 	})
-// 	if err := client.Ping().Err(); err != nil {
-// 		return nil, err
-// 	}
-// 	return &Database{
-// 		Client: client,
-// 	}, nil
-// }
-
 func main() {
 	router := gin.Default()
 	router.RedirectTrailingSlash = true
 
-	// client := redis.NewClient(&redis.Options{
-	// 	Addr:     "localhost:6379",
-	// 	Password: "",
-	// 	DB:       0,
-	// })
+	u := redisdb.InitUserRedis()
+	c := redisdb.InitDestRedis()
+	a := redisdb.InitAutoCompleterRedis()
 
-	// Create schema
-	sc := redisearch.NewSchema(redisearch.DefaultOptions)
-	sc.AddField(redisearch.NewTextFieldOptions("destination", redisearch.TextFieldOptions{Sortable: true}))
-	sc.AddField(redisearch.NewTextFieldOptions("uid", redisearch.TextFieldOptions{Sortable: true}))
-
-	c := redisearch.NewClient("localhost:6379", "redisearch")
-	err := c.Drop()
-	// Create the index with the given schema
+	fmt.Println("Autocomplete", redisdb.AutoCompleteDestination(a, "Sing"))
+	fmt.Println(redisdb.GetDestinationUid(c, "Singapore"))
+	output, err := c.Get(redisdb.GetDestinationUid(c, "Singapore"))
 	if err != nil {
 		fmt.Println(err)
 	}
-	c.CreateIndex(sc)
+	fmt.Println(output.Properties["uid"])
 
-	a := redisearch.NewAutocompleter("localhost:6379", "autocomplete")
-	// destinationSuggest := redisearch.NewAutocompleter(RedisAddr, "destinations")
-
-	// destinationSuggest.SuggestOpts("ro", redisearch.SuggestOptions{Num: 5, Fuzzy: true})
-
-	content, err := os.Open("./destinations.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("Successfully read file")
-	defer content.Close()
-	var destinations []Destination
-	// var uids []Uid
-
-	bytevalue, _ := ioutil.ReadAll(content)
-	json.Unmarshal(bytevalue, &destinations)
-	// json.Unmarshal(bytevalue, &uids)
-
-	for i := 0; i < 150; i++ {
-		if destinations[i].Dest == "" || destinations[i].Uid == "" {
-			continue
-		}
-		a.AddTerms(redisearch.Suggestion{Term: destinations[i].Dest, Score: 1})
-		doc := redisearch.NewDocument("destination:"+strconv.Itoa(i), 1.0)
-		doc.Set("destination", destinations[i].Dest)
-		doc.Set("uid", destinations[i].Uid)
-		if err := c.Index([]redisearch.Document{doc}...); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	fmt.Println("Successfully indexed")
-	docs, total, err := c.Search(redisearch.NewQuery("Rome, Italy").SetReturnFields("destination").Limit(0, 5))
-	for i := 0; i < len(docs); i++ {
-		fmt.Println(docs[i].Id, docs[i].Properties["destination"], total, err)
-	}
-
-	// return autcomplete results
-	fmt.Println(a.SuggestOpts("Bal", redisearch.SuggestOptions{Num: 5, Fuzzy: false}))
-	fmt.Println(a.Length())
-
-	// fmt.Println(destinationSuggest.SuggestOpts("Ro", redisearch.SuggestOptions{Num: 5, Fuzzy: true}))
-
-	// ***
-	// USELESS FOR NOW
-
-	// pong, err := client.Ping().Result()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(pong, "connected to redis")
-
-	// client.Set("destinations", bytevalue, 0)
-
-	// destinationJsondata, _ := json.Marshal(destinations)
-
-	// client.Set("destinations", destinationJsondata, 0).Err()
-
-	// uidJsondata, _ := json.Marshal(uids)
-
-	// client.Set("uids", uidJsondata, 0)
-
-	// val, err := client.Get("destinations").Result()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println("destinations:", val)
-
-	// err = client.Set("destinations", destinations, 0).Err()
-	// if err != nil {
-	// 	fmt.Println(err)
+	redisdb.AddNewUser(u, "rktmeister2", "1234")
+	redisdb.AddNewUser(u, "rktmeister1", "1234")
+	// USE REDISEARCH TO FIND UID AS WELL
+	// docs, total, err := c.Search(redisearch.NewQuery("Rome, Italy").SetReturnFields("uid").Limit(0, 5))
+	// for i := 0; i < len(docs); i++ {
+	// 	fmt.Println(docs[i].Id, docs[i].Properties["uid"], total, err)
 	// }
 
-	// val, err := client.Get("destinations").Result()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println("destinations:", val)
-
-	// ***
-
-	// destinationSuggest := redisearch.NewAutocompleter(RedisAddr, "destinations")
-	// fmt.Println("Sucessfully create autocompleter")
-	// fmt.Println(destinationSuggest.SuggestOpts("Ro", redisearch.SuggestOptions{Num: 5, Fuzzy: true}))
+	// // return autcomplete results
+	// // a.SuggestOpts first argument will be taken from the GET REQUEST, for now not yet implemented
+	// fmt.Println(a.SuggestOpts("Bal", redisearch.SuggestOptions{Num: 5, Fuzzy: false}))
+	// fmt.Println(a.Length())
 
 	api := router.Group("/api")
 	{
