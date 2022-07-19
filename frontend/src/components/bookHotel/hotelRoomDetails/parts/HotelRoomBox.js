d6940802dc75274728efe7de6c150d2ff6eac1ec
Jonah <100175129+J-onah@users.noreply.github.com>
2022-07-19 16:01:53 +0800

    Update HotelRoomBox.js

--- frontend/src/components/bookHotel/hotelRoomDetails/parts/HotelRoomBox.js
+++ frontend/src/components/bookHotel/hotelRoomDetails/parts/HotelRoomBox.js
@@ -1,22 +1,28 @@
-const HotelRoomBox = (props) => {
+import LowerCaseChange from "./LowerCaseChange.js";
+import { useState } from "react";
+import './../HotelRoomDetails.css';
 
-    return (
+function HotelRoomBox(props){
 
-        <div className="HotelRoomBox">
-            <br></br>
+    const [description, setDescription] = useState(props.description);
+    const price = props.price;
 
-            <span className="HotelRoomBoxLeft">{props.description}</span>
-            {<span>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>}
-            <span className="HotelRoomBoxRight">{props.price}</span>
+    return(
+    
+        <div className = "HotelRoomBox">
+            <br></br>
+            
+            <span className = "HotelRoomBoxLeft">{LowerCaseChange(description)}</span>
+            {<span>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;</span>} 
+            <span className = "HotelRoomBoxRight">${price}</span>
             <br></br>
             <br></br>
             <br></br>
             <br></br>
-
-
+        
         </div>
-    );
+    )
+  
+}
 
-};
-
-export default HotelRoomBox;
+export default HotelRoomBox
